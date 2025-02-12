0aa03d3e9c7200e37191504840a2351814c9cea6
Maria Parra <maria.parra05@usa.edu.co>
2025-02-12 08:18:39 -0500

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
- int factorial(int n) {
+ int factorialR(int n) {
     if (n <= 1)
         return 1;
-    return n * factorial(n - 1);
+    return n * factorialR(n - 1);
  }
+ 
+ int factorialI(int n) {
+     int resultado=1;
+     for (int i=2; i<=n; i++){
+         resultado*=i;
+     }
+     return resultado;
+ }
+ 
  int main(void) {
     int n = 5;
-    int factorialNumero = factorial(n);
-    printf("El factorial recursivo de %llu es %llu", n, factorialNumero);
+    int factorialRNumero = factorialR(n);
+    int factorialINumero = factorialI(n);
+    printf("El factorial recursivo de %llu es %llu \n", n, factorialRNumero);
+    
+    printf("El factorial iterativo de %llu es %llu", n, factorialINumero);
 }
